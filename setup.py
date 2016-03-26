67c3774ba2bdf5a9b1c79f0145b265550cf879ed
Petr Dlouhý <petr.dlouhy@email.cz>
2016-03-26 07:02:14 +0100

    initial version of setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup  # noqa
+
+
+setup(name="django-compressor-sourcemaps",
+      version="0.1",
+      description="Source maps for django compressor",
+      long_description=open("README.md").read(),
+      author="Philip Kimmey",
+      author_email="",
+      url="https://github.com/roverdotcom/django-compressor-sourcemaps",
+      download_url="https://github.com/roverdotcom/django-compressor-sourcemaps/archive/master.zip",
+      classifiers=[
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Python Software Foundation License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Internet",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4"
+          ])
